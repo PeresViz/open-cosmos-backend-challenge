6d06e5eb41b2f822973188ad0ee72cb4076dacc3
peres <tiago.sousa@viz.ai>
2024-03-30 18:33:24 +0000

    Replaced some hardcoded string in redis_data_storage.py by constants

--- infrastructure/data/redis_data_storage.py
+++ infrastructure/data/redis_data_storage.py
@@ -2,26 +2,33 @@ import redis
 import json
 from typing import Any
 from infrastructure.data.abstract_data_storage import AbstractDataStorage
+from infrastructure.data.constants import (
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_DB_NUMBER,
+    REDIS_DATA_HASH_KEY,
+    REDIS_DISCARD_REASONS_HASH_KEY,
+)
 
 
 class RedisDataStorage(AbstractDataStorage):
     def __init__(self):
-        self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
+        self.redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB_NUMBER)
 
     def save_data(self, data: dict[str, Any]):
-        self.redis_client.hset('data', data['time'], json.dumps(data))
+        self.redis_client.hset(REDIS_DATA_HASH_KEY, data['time'], json.dumps(data))
 
     def get_data(self):
-        data = self.redis_client.hgetall('data')
+        data = self.redis_client.hgetall(REDIS_DATA_HASH_KEY)
         return [json.loads(data_json) for data_json in data.values()]
 
     def save_discard_reasons(self, discard_reasons: list[dict[str, Any]]):
         for discard_reason in discard_reasons:
             timestamp = discard_reason['timestamp']
             reason = discard_reason['reason']
-            self.redis_client.hset('discard_reasons', timestamp, reason)
+            self.redis_client.hset(REDIS_DISCARD_REASONS_HASH_KEY, timestamp, reason)
 
     def get_discard_reasons(self):
-        discard_reasons = self.redis_client.hgetall('discard_reasons')
+        discard_reasons = self.redis_client.hgetall(REDIS_DISCARD_REASONS_HASH_KEY)
         reasons_list = [{"timestamp": timestamp.decode(), "reason": reason.decode()} for timestamp, reason in discard_reasons.items()]
         return reasons_list
