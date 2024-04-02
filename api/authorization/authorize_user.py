f75162323399252311c7cf0da78ebabda474a1ab
peres <tiago.sousa@viz.ai>
2024-04-02 09:57:11 +0100

    Renaming authentication files to authorization

--- api/authorization/authorize_user.py
+++ api/authorization/authorize_user.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 
 
-async def authenticate_user(api_key: str):
+async def authorize_user(api_key: str):
     if api_key == "admin_api_key":
         return "admin"
     elif api_key == "user_api_key":
--- api/decorators/requires_permissions.py
+++ api/decorators/requires_permissions.py
@@ -1,13 +1,13 @@
 from fastapi import HTTPException, Depends
 from datetime import datetime
 
-from api.authentication.authenticate_user import authenticate_user
-from api.authentication.roles import ROLES
+from api.authorization.authorize_user import authorize_user
+from api.authorization.roles import ROLES
 
 
 def requires_permissions(required_permissions: list):
     def decorator(func):
-        async def wrapper(user_role: str = Depends(authenticate_user), start_time: datetime = None, end_time: datetime = None):
+        async def wrapper(user_role: str = Depends(authorize_user), start_time: datetime = None, end_time: datetime = None):
             if not set(required_permissions).issubset(ROLES.get(user_role, [])):
                 raise HTTPException(status_code=403, detail="Insufficient permissions")
             return await func(start_time=start_time, end_time=end_time)
